58f03f476985b9a6cfc1eae998c73597c4bfd2e4
siraknorbert <siraknorbert99@gmail.com>
2021-04-14 23:21:25 +0200

    Osztályok - PlayerController hozzáadása

---
+++ private/Classes/PlayerController.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+using PlayerModel;
+
+public class PlayerController : MonoBehaviour
+{
+	[SerializableField] private RigidBody2D rigidBody;
+	[SerializableField] private Animator playerAnimation;
+	private PlayerModel player;
+
+    void Start ()
+    {
+        rigidBody = GetComponent<RigidBody2D>();
+        playerAnimation = GetComponent<Animator>();
+        player = new PlayerModel();
+        PlayerModel.SetLifesAtLevelBegin();
+    }
+  
+    
+    void Update ()
+    {
+        
+    }
+}

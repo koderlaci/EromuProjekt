ba8e13bacf7dfd2fc190d5215a38693f18cc51e0
CsontosDominik <61829086+CsontosDominik@users.noreply.github.com>
2021-04-14 21:44:51 +0200

    Prototípus elkészítése: Karakter ugrás  script probléma megoldások
    Egy visual studióban fellépő hiba miatti proglémán való dolgozás

---
+++ private/Eromu/Assets/mozgas.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mozgas : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float sebesség = 7f;
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        Jump();
+        Vector3 mozgás = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += mozgás * Time.deltaTime * sebesség;
+    }
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            gameObject.GetComponent<Rigidbody2D>().Addforce(new Vector2(0f, 5f), ForceMode2D.Impulse);
+        }
+    }
+}

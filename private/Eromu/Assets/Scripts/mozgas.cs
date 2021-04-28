3ff64ff788b20f2941d02fd435762bb38f8d7e70
koderlaci <koder.laci67@gmail.com>
2021-04-28 18:37:52 +0200

    Mappák rendezése

---
+++ private/Eromu/Assets/Scripts/mozgas.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mozgas : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float sebesség = 7f;
+    public float magasság = 5f;
+    void Start()
+    {
+
+    }
+
+    // Update
+    void Update()
+    {
+        Jump();
+        Vector3 mozgás = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += mozgás * Time.deltaTime * sebesség;
+    }
+
+    // Jump
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, magasság), ForceMode2D.Impulse);
+        }
+    }
+}

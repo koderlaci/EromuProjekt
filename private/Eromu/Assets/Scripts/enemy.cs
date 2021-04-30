c3787dee2e1c917f01d0dbdec19b21f4fc4e8a59
CsontosDominik <61829086+CsontosDominik@users.noreply.github.com>
2021-04-30 14:37:39 +0200

    Ellenség mozgásának implementálása

---
+++ private/Eromu/Assets/Scripts/enemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+
+    public float speed;
+    public bool MoveRight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (MoveRight)
+        {
+            transform.Translate(2 * speed * Time.deltaTime, 0, 0);
+        }
+        else
+        {
+            transform.Translate(-2 * speed * Time.deltaTime, 0, 0);
+        }
+
+
+    }
+}

eddf8aafc7f91a6f027d5f9bd9c6cec22ce554dc
CsontosDominik <61829086+CsontosDominik@users.noreply.github.com>
2021-05-06 14:14:33 +0200

    Fall detector Box collider hozzáadása

--- private/Eromu/Assets/Scripts/Playerdeath.cs
+++ private/Eromu/Assets/Scripts/Playerdeath.cs
@@ -4,24 +4,16 @@ using UnityEngine;
 
 public class Playerdeath : MonoBehaviour
 {
+    public GameObject player; 
     // Start is called before the first frame update
     void Start()
     {
-      
     }
 
     // Update is called once per frame
     void Update()
     {
-
     }
     //karakter hozzáér a megfelelő triggerhez és emmiatt a poziciója visszakerül a start ra
-    void OnTriggerEnter2D(Collider2D collision)
-    {
-        //collision.gameObject.CompareTag("Death")
-        if (transform.position.x==4)
-        {
-            transform.position = new Vector3(-5,-2,0);
-        }
-    }
+    
 }

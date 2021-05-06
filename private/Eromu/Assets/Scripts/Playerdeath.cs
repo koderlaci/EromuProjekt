2306e984c99023b8a61dd36847834c26580eff05
CsontosDominik <61829086+CsontosDominik@users.noreply.github.com>
2021-05-06 13:40:46 +0200

    Fall detector player death script megírása

---
+++ private/Eromu/Assets/Scripts/Playerdeath.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playerdeath : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    //karakter hozzáér a megfelelő triggerhez és emmiatt a poziciója visszakerül a start ra
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        //collision.gameObject.CompareTag("Death")
+        if (transform.position.x==4)
+        {
+            transform.position = new Vector3(-5,-2,0);
+        }
+    }
+}

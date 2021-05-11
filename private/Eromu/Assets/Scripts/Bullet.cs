9d98bfca768917ca3dc2edf7b5be1ef6f721c2e0
koderlaci <koder.laci67@gmail.com>
2021-05-11 16:58:31 +0200

    Lövés sebzés hozzáadása

--- private/Eromu/Assets/Scripts/Bullet.cs
+++ private/Eromu/Assets/Scripts/Bullet.cs
@@ -6,16 +6,24 @@ public class Bullet : MonoBehaviour
 {
     public float speed = 20f;
     public Rigidbody2D rb;
+    public int damage = 40;
+    public GameObject impactEffect;
 
-    // Start is called before the first frame update
     void Start()
     {
         rb.velocity = transform.right * speed;
     }
 
-    // Update is called once per frame
-    void Update()
+    void Trigger(Collider2D Hit)
     {
-        
+        //Enemy enemy = Hit.GetComponent<Enemy>();
+        //if (enemy != null)
+        //{
+        //    enemy.TakeDamage(damage);
+        //}
+
+        Instantiate(impactEffect, transform.position, transform.rotation);
+
+        Destroy(gameObject);
     }
 }

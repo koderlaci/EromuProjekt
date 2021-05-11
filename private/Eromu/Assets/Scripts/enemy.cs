4512426d16daa406ea4b347b920ca51ae030bdd9
koderlaci <koder.laci67@gmail.com>
2021-05-11 17:01:55 +0200

    Ellenség sebződésének beállítása

--- private/Eromu/Assets/Scripts/enemy.cs
+++ private/Eromu/Assets/Scripts/enemy.cs
@@ -4,42 +4,61 @@ using UnityEngine;
 
 public class enemy : MonoBehaviour
 {
+    public int health = 100;
 
-    public float speed;
-    public bool MoveRight;
-    // Start is called before the first frame update
-    void Start()
+    public GameObject deathEffect;
+
+    public void TakeDamage(int damage)
     {
-        
+        health -= damage;
+
+        if (health <= 0)
+        {
+            Die();
+        }
     }
+
+    void Die()
+    {
+        Instantiate(deathEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+
+    //public float speed;
+    //public bool MoveRight;
+    // Start is called before the first frame update
+    //void Start()
+    //{
+        
+    //}
 
     // Update is called once per frame
-    void Update()
-    {
-        if (MoveRight)
-        {
-            transform.Translate(2 * speed * Time.deltaTime, 0, 0);
-        }
-        else
-        {
-            transform.Translate(-2 * speed * Time.deltaTime, 0, 0);
-        }
-    }
+    //void Update()
+    //{
+    //    if (MoveRight)
+    //    {
+    //        transform.Translate(2 * speed * Time.deltaTime, 0, 0);
+    //    }
+    //    else
+    //    {
+    //        transform.Translate(-2 * speed * Time.deltaTime, 0, 0);
+    //    }
+    //}
 
-    void OnTriggerEnter2D(Collider2D trig)
-    {
-        if (trig.gameObject.CompareTag("Turn"))
-        {
-            if (MoveRight)
-            {
-                MoveRight = false;
-            }
-            else
-            {
-                MoveRight = true;
-            }
-        }
-    }
+    //void OnTriggerEnter2D(Collider2D trig)
+    //{
+    //    if (trig.gameObject.CompareTag("Turn"))
+    //    {
+    //        if (MoveRight)
+    //        {
+    //            MoveRight = false;
+    //        }
+    //        else
+    //        {
+    //            MoveRight = true;
+    //        }
+    //    }
+    //}
 }
 
 

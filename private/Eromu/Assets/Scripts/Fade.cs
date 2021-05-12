b6fd4a5c66c5555d9efadb29e7ca7a59dafdfd8b
koderlaci <koder.laci67@gmail.com>
2021-05-12 15:52:52 +0200

    FadeIn Implementálása

---
+++ private/Eromu/Assets/Scripts/Fade.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fade : MonoBehaviour
+{
+    public Image Fadeimg;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Fadeimg.canvasRenderer.SetAlpha(1.0f);
+        FadeIn();
+    }
+
+    // Update is called once per frame
+    void FadeIn()
+    {
+        Fadeimg.CrossFadeAlpha(0,10,false);
+    }
+}
